353914c20120b7a869f3b87e9ce561590fabb915
haruki7049 <tontonkirikiri@gmail.com>
2025-05-27 17:28:31 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use clap::Parser;
 use std::path::PathBuf;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _args: CLIArgs = CLIArgs::parse();
-    todo!()
+    let args: CLIArgs = CLIArgs::parse();
+
+    // for Rust's panic debugging
+    color_eyre::install()?;
+
+    // Set up terminal
+    let terminal = ratatui::init();
+    run(terminal)?;
+    ratatui::restore();
+
+    Ok(())
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
 
 #[derive(Debug, Parser)]
