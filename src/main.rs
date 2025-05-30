9ff358e7f9d1b5c883fa9369e8a0b231459f52ca
haruki7049 <tontonkirikiri@gmail.com>
2025-05-30 16:05:10 +0900

    style: nix fmt

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ struct Config;
 
 impl Config {
     fn load() -> Result<(), Box<dyn std::error::Error>> {
-        let proj_dirs = ProjectDirs::from("dev", "haruki7049", "Core").ok_or("CONFIG_LOAD_ERROR: Failed to create Project Directories");
+        let proj_dirs = ProjectDirs::from("dev", "haruki7049", "Core")
+            .ok_or("CONFIG_LOAD_ERROR: Failed to create Project Directories");
         proj_dirs?.config_dir();
 
         Ok(())
