e54a68a10f6bdc173c2c6cc99451ea1ee78f939a
haruki7049 <tontonkirikiri@gmail.com>
2025-05-30 16:03:40 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use directories::ProjectDirs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    todo!()
+    Config::load()?;
+    todo!();
+}
+
+struct Config;
+
+impl Config {
+    fn load() -> Result<(), Box<dyn std::error::Error>> {
+        let proj_dirs = ProjectDirs::from("dev", "haruki7049", "Core").ok_or("CONFIG_LOAD_ERROR: Failed to create Project Directories");
+        proj_dirs?.config_dir();
+
+        Ok(())
+    }
 }
