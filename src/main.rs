5853ef4c2ce419b78324227115bcba5a26f9bb46
haruki7049 <tontonkirikiri@gmail.com>
2025-06-04 07:11:35 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,19 @@ use directories::ProjectDirs;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config: Config = Config::load()?;
-    dbg!(&config);
+    let ast = load_config()?;
+    dbg!(&ast);
 
     Ok(())
 }
 
-#[derive(Debug)]
-struct Config {
-    path: PathBuf,
-    context: AST,
-}
+fn load_config() -> Result<AST, Box<dyn std::error::Error>> {
+    let proj_dirs = ProjectDirs::from("dev", "haruki7049", "Core")
+        .ok_or("CONFIG_LOAD_ERROR: Failed to create Project Directories")?;
 
-impl Config {
-    fn load() -> Result<Self, Box<dyn std::error::Error>> {
-        let proj_dirs = ProjectDirs::from("dev", "haruki7049", "Core")
-            .ok_or("CONFIG_LOAD_ERROR: Failed to create Project Directories")?;
+    let config_path: PathBuf = PathBuf::from(proj_dirs.config_dir()).join("init.core");
+    let config: String = std::fs::read_to_string(&config_path).unwrap_or_default();
+    let result: AST = evaluator::eval(&config)?;
 
-        let config_path: PathBuf = PathBuf::from(proj_dirs.config_dir()).join("init.core");
-        let config: String = std::fs::read_to_string(&config_path).unwrap_or_default();
-        let ast: AST = evaluator::eval(&config)?;
-
-        Ok(Config {
-            path: config_path,
-            context: ast,
-        })
-    }
+    Ok(result)
 }
